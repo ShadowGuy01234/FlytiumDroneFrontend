91de788d542bdb2ff05c51354057d7fc37fcb4db
Flytium-drones <ankitkumar78007080@gmail.com>
2025-01-24 16:58:29 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBw4ft3TMgCN9Jeez8oH52RmJ_9aVbOR7E",
-    authDomain: "flytiumdrone.firebaseapp.com",
-    projectId: "flytiumdrone",
-    storageBucket: "flytiumdrone.firebasestorage.app",
-    messagingSenderId: "707648745393",
-    appId: "1:707648745393:web:6195ba432f1a52a0b71790",
-    measurementId: "G-W4WFG3WVNS"
+ apiKey: "AIzaSyBr0zU6Qwjz8HbL5jQLt3y4g35z2SDpoNM",
+  authDomain: "flytiumdrone-5d4e0.firebaseapp.com",
+  projectId: "flytiumdrone-5d4e0",
+  storageBucket: "flytiumdrone-5d4e0.firebasestorage.app",
+  messagingSenderId: "338102964159",
+  appId: "1:338102964159:web:f4787c2a46f817c7fddb0a",
+  measurementId: "G-V07WWCDY6Z"
 };
 
 const app = initializeApp(firebaseConfig);
